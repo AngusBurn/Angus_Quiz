74e92f7b2dea9843ce8c59596a9b6ad3b9d7b011
Angus Burn <angusburn12@gmail.com>
2023-09-12 15:08:33 +1000

    Added code to load a csv file using pandas. Added code that accesses every row under specific column labels to store them in variables to be shown on graphs.

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+df = pd.read_csv("climate.csv")
+
+years = df.loc[:,["Year"]]
+co2 = df.loc[:,["CO2"]]
+temp = df.loc[:,["Temperature"]]
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
