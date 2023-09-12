72a6ae83be2ed3d5a6a8c28ee1fe9f44e66e3893
Angus Burn <angusburn12@gmail.com>
2023-09-12 14:43:56 +1000

    Added code to impot sqlite3, added cursor and connection code, added sql_command code to select from database table, added execute code and loop to update provided lists with database rows.

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+sql_command = """
+SELECT Year, CO2, Temperature FROM ClimateData
+"""
+
+cursor.execute(sql_command)
+rows = cursor.fetchall()
+
+for row in rows:
+    year, co2_record, temp_record = row
+    years.append(year)
+    co2.append(co2_record)
+    temp.append(temp_record)
+
+connection.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
